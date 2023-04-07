898767340bb43d3d9bfa47bac1fcb16be30bafbc
Satveer1987 <103417397+Satveer1987@users.noreply.github.com>
2023-04-07 10:39:12 +0530

    Update j-ip-address.py

--- Chapter-9-Dictionaries/Exercise/B/j-ip-address.py
+++ Chapter-9-Dictionaries/Exercise/B/j-ip-address.py
@@ -15,6 +15,20 @@ WAP to perfrom the following operations:
 - Add two new entries to the dictionary
 """
 
-interfaces = {('eth0', '1.1.1.1'): 'up', ('eth1', '2.2.2.2'): 'up', ('wlan0', '3.3.3.3') : 'down', ('wlan1', '4.4.4.4'): 'up'}
+ifs = {'eth0':{'ip':'1.1.1.1','status': 'up'}, 'eth1':{'ip': '2.2.2.2','status': 'up'}, 
+       'wlan0':{'ip':'3.3.3.3','status' : 'down'}, 'wlan1':{'ip': '4.4.4.4','status': 'up'}}
+print()
+test=input('enter an interface:')
+print(ifs[test]['status'])
 
-interface = input('Enter the interface: ')
+for k,v in ifs.items():
+    if v['status']=='up':
+        print(k,v['ip'])
+print()
+print('total interfaces:',len(ifs))
+
+print()
+ifs['eth2']={'ip':'5.5.5.5','status' : 'up'}
+ifs['wlan2']={'ip':'6.6.6.6','status' : 'down'}
+for k,v in ifs.items():
+    print(k,v)
